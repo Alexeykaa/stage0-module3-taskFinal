df6ee11e5505ca09a3b036581f906697211d1d1b
Alexeykaa <ka.alexey@gmail.com>
2023-10-13 23:55:48 +0300

    init implementation

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,12 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        // int sum = number / 1000 + number % 1000 / 100 + number % 100 / 10 + number % 10;
+        int sum = 0;
+        while (number > 0) {
+            sum += number % 10;
+            number /= 10;
+        }
+        System.out.println(sum);
     }
 }
